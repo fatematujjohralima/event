eb4634ed95986f640b69a2073482ffdd6a53d67f
fatematujjohra <fatematujjohralima2@gmail.com>
2026-03-08 22:57:35 +0600

    Update name on button click

--- js/events.js
+++ js/events.js
@@ -2,3 +2,11 @@ document.getElementById('login').addEventListener('click',function () {
     const user=document.getElementById('user');
     user.innerText='User Logged In Successfully';
 })
+
+document.getElementById('update-btn').addEventListener('click',function(){
+    const inputName=document.getElementById('input-name');
+    const updateName=inputName.value;
+    const name=document.getElementById('name');
+    name.innerText=updateName;
+
+})
